8523e7a97de6f3df941cee94edf2624824765b67
André Mathisen <andre.mathisen@bouvet.no>
2026-02-20 14:58:54 +0100

    Create TodoCreateDto.cs

---
+++ MyTodoApp.API/Models/TodoCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.API.Models;
+
+public class TodoCreateDto
+{
+    [Required(ErrorMessage = "Title is required.")]
+    [MaxLength(50)]
+    public required string Title { get; set; }
+    [MaxLength(100)]
+    public string? Description { get; set; }
+}
